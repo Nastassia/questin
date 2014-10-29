cf5e000cd342d6335a8bd04683afd26f0dce8089
Nastassia <nastassiacarmona@gmail.com>
2014-10-29 06:24:51 -0400

    Added active record association

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,5 @@
 class Character < ActiveRecord::Base
   belongs_to :user
+  self.has_one :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  # validates :username, :password_digest, presence :true
+  self.has_many :characters
 
 end
